7a69560835c75c3a7dd0ac33c03a8770daa9c487
Suomi228 <bixep345@gmail.com>
2024-08-16 22:09:51 +0300

    crud

---
+++ models/Message.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose'
+const messageSchema = mongoose.Schema({
+ message: String,
+ name: String,
+ timestamp: String,
+ received: Boolean
+})
+export default mongoose.model("Message", messageSchema);
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import Messages from "./models/Message.js";
+import Cors from "cors"
 dotenv.config();
 const app = express();
+app.use(express.json())
+app.use(Cors())
 const port = process.env.PORT || 9000;
 app.get("/", (req, res) => res.status(200).send("Hello TheWebDev"));
 mongoose
@@ -13,5 +17,24 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+  Messages.create(dbMessage)
+    .then((data) => {
+      res.status(201).send(data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+app.get("/messages/sync", (req, res) => {
+  Messages.find()
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
 app.listen(port, () => console.log(`Listening on localhost: ${port}`));
